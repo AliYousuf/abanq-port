9741c14549ba537316ec2fd285bc89d08a71d86f
project.ufa <project.ufa@9bbcd52c-0291-11de-9d1a-b59b2e1864b6>
2009-02-27 16:30:44 +0000

    rama para compilar qsa mediante un wrapper

--- abanq-port/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the facturalux package
-#
-0 4	* * *	root	facturalux_maintenance
--- abanq-port/src/plugins/styles/styles.pro
+++
@@ -1,5 +0,0 @@
-TEMPLATE = subdirs 
-CONFIG += warn_off 
-SUBDIRS += bluecurve
-!win32:SUBDIRS += kde
-
