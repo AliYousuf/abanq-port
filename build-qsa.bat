4ed9dc9715a236b5f6cb0c0e7e5147e5a8add688
project.ufa <project.ufa@9bbcd52c-0291-11de-9d1a-b59b2e1864b6>
2009-02-27 19:47:29 +0000

    agregado el script para compilar qsa

---
+++ build-qsa.bat
@@ -0,0 +1,22 @@
+@echo off
+echo "compiling qsa..."
+set MAKE=mingw32-make
+::cd src\qsa
+
+echo building configure2...
+cd configure2
+qmake -spec %QMAKESPEC% -o Makefile configure2.pro
+%MAKE%
+  
+echo executing configure2...
+cd ..
+"configure2\configure2.exe"
+
+echo coping QT cache...
+copy /Y %QTDIR%\.qmake.cache .qmake.cache
+copy /Y %QTDIR%\.qmake.cache src\qsa\
+copy /Y %QTDIR%\.qmake.cache src\plugin\
+
+echo generating Makefile...
+qmake -o Makefile qsa.pro
+%MAKE%
