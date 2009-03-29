3c9529f4daa9a218742002e4edda0578a5876b20
project.ufa <project.ufa@9bbcd52c-0291-11de-9d1a-b59b2e1864b6>
2009-03-29 18:01:46 +0000

    agregado de qDebug con mensajes para cada funcion

--- src/flbase/FLFiscalBixolon.cpp
+++ src/flbase/FLFiscalBixolon.cpp
@@ -2,50 +2,58 @@
 
 FLFiscalBixolon::FLFiscalBixolon()
 {
-
+  qDebug("FLFiscalBixolon Constructor");
 }
 
 FLFiscalBixolon::~FLFiscalBixolon()
 {
-
+  qDebug("FLFiscalBixolon Destructor");
 }
 
 bool FLFiscalBixolon::openPort( QString port )
 {
+  qDebug(QString("FLFiscalBixolon openPort(%1)").arg(port).ascii());
   return true;
 }
 
 bool FLFiscalBixolon::closedPort()
 {
+  qDebug("FLFiscalBixolon closedPort");
   return true;
 }
 
 bool checkPrinter()
 {
+  qDebug("FLFiscalBixolon checkPrinter");
   return true;
 }
 
 bool FLFiscalBixolon::readStatus( int * status, int * error )
 {
+  qDebug("FLFiscalBixolon readStatus");
   return true;
 }
 
 bool FLFiscalBixolon::sendCmd( int * status, int * error, QString cmd )
 {
+  qDebug("FLFiscalBixolon sendCmd");
   return true;
 }
 
 int FLFiscalBixolon::sendNcmd( int * status, int * error, QString cmd )
 {
+  qDebug("FLFiscalBixolon sendNcmd");
   return 0;
 }
 
 int FLFiscalBixolon::sendFile( int * status, int * error, QString file )
 {
+  qDebug("FLFiscalBixolon SendFile");
   return 0;
 }
 
 bool FLFiscalBixolon::uploadStatus( int * status, int * error, char *cmd, QString file )
 {
+  qDebug("FLFiscalBixolon uploadStatus");
   return true;
 }
